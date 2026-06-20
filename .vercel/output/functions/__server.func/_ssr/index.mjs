81f3cd13e7efa39fb16be3bb27bbf30ce0e703ef
techmonk05 <chkarthik2328@gmail.com>
2026-06-20 16:48:16 +0530

    vercel changes

--- .vercel/output/functions/__server.func/_ssr/index.mjs
+++ .vercel/output/functions/__server.func/_ssr/index.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./server-CGGkk59Q.mjs").then((n) => n.s).then(
+    serverEntryPromise = import("./server-Xh6NsCh8.mjs").then((n) => n.s).then(
       (m) => m.default ?? m
     );
   }
